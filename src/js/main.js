86e8a412698c380b75ecbde54092713bb3cabd4d
Christian Baltazar <christian.baltazar@drivek.com>
2020-04-11 14:40:32 +0200

    chore(mais.js): update items

--- src/js/main.js
+++ src/js/main.js
@@ -22,12 +22,29 @@ new Vue({
 
             this.$http.post(this.endpoint.save, data).then(
                 response => {
-
+                    this.update(item, target);
                 },
                 error => {
                     alert((error.bodyText || error.body || error));
                 }
             )
+        },
+        update(item, target) {
+            let update = null;
+            switch (target) {
+                case 'types':
+                    update = this.types.filter(type => type.id == item.id);
+                    break;
+                case 'categories':
+                    update = this.categories.filter(cat => cat.id == item.id);
+                    break;
+                case 'tags':
+                    update = this.tags.filter(tag => tag.id == item.id);
+                    break;
+            }
+
+            update.active = ! item.active;
+
         }
     },
     created() {
