6e23c46f3a32a61c788fba688090d3ddc5bd5043
Christian Baltazar <christian.baltazar@drivek.com>
2020-04-11 13:20:58 +0200

    chore(Controller): set custom order

--- src/Controller.php
+++ src/Controller.php
@@ -8,6 +8,9 @@ namespace PostVisitsWizard;
  */
 class Controller {
 
+	/**
+	 *
+	 */
 	const META_COUNTER = '_post_visits_counter';
 	/**
 	 * @var Settings
@@ -28,9 +31,13 @@ class Controller {
 	 */
 	public function run() {
 		add_action( 'wp', [ $this, 'register_visit' ], 10 );
+		add_action( 'pre_get_posts', [ $this, 'set_custom_order' ], 10, 1 );
 	}
 
 
+	/**
+	 *
+	 */
 	public function register_visit() {
 		if ( ! is_single() || is_admin() ) {
 			return;
@@ -41,5 +48,28 @@ class Controller {
 		$counter = get_post_meta( $post_id, self::META_COUNTER, true ) ?: 0;
 
 		update_post_meta( $post_id, self::META_COUNTER, ++ $counter );
+	}
+
+	/**
+	 * @param $query
+	 */
+	public function set_custom_order( $query ) {
+		if ( ( ! is_category() || ! is_archive() ) || ! $query->is_main_query() ) {
+			return;
+		}
+
+		$query->query_vars['order']      = 'DESC';
+		$query->query_vars['orderby']    = 'meta_value date';
+		$query->query_vars['meta_query'] = [
+			'relation' => 'OR',
+			[
+				'key'     => self::META_COUNTER,
+				'compare' => 'EXISTS'
+			],
+			[
+				'key'     => self::META_COUNTER,
+				'compare' => 'NOT EXISTS'
+			]
+		];
 	}
 }
