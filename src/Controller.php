38cec34f0b2c857c782560bb850e7b506f355968
Christian Baltazar <christian.baltazar@drivek.com>
2020-04-12 12:21:01 +0200

    chore(Controller) set post table data

--- src/Controller.php
+++ src/Controller.php
@@ -9,6 +9,9 @@ namespace PostVisitsWizard;
 class Controller {
 
 	const META_COUNTER = '_post_visits_counter';
+
+	const POST_TABLE_COLUMN = 'Total visits';
+
 	/**
 	 * @var Settings
 	 */
@@ -29,8 +32,43 @@ class Controller {
 	public function run() {
 		add_action( 'wp', [ $this, 'register_visit' ], 10 );
 		add_action( 'pre_get_posts', [ $this, 'set_custom_order' ], 10, 1 );
+
+		foreach ( $this->settings['types'] as $cpt ) {
+			$filter_column = sprintf( 'manage_%s_posts_columns', $cpt );
+			$filter_data   = sprintf( 'manage_%s_posts_custom_column', $cpt );
+
+			if ( $cpt === 'post' ) {
+				$filter_column = str_replace( '_posts_', '', $filter_column );
+				$filter_data   = str_replace( '_posts_', '', $filter_data );
+			}
+
+			add_filter( $filter_column, [ $this, 'add_post_table_column' ], 10, 1 );
+			add_action( $filter_data, [ $this, 'set_post_column_data' ], 10, 2 );
+		}
 	}
 
+	/**
+	 * @param $columns
+	 *
+	 * @return array
+	 */
+	public function add_post_table_column( $columns ) {
+		$columns[] = self::POST_TABLE_COLUMN;
+
+		return $columns;
+	}
+
+	/**
+	 * @param $column
+	 * @param $post_id
+	 */
+	public function set_post_column_data( $column, $post_id ) {
+		if ( $column !== self::POST_TABLE_COLUMN ) {
+			return;
+		}
+
+		echo get_post_meta( $post_id, self::META_COUNTER, true ) ?: 0;
+	}
 
 	/**
 	 *
